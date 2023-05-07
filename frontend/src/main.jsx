b7c42b3c6a1d905c4bb2a08b8df6f96a1fe6a9b8
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-05-07 04:54:34 -0700

    Removed Van Context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AuthContextProvider } from './context/AuthContext'
-import { VanContextProvider } from './context/VanContext'
 
 import App from './App'
 import './index.css'
@@ -9,9 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-    <VanContextProvider>
       <App />
-    </VanContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
